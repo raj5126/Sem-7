43fdd7811f5ecda85c68ae12cca3855bb6832a8a
raj/5126 <panchalraj544@gmail.com>
2020-07-14 19:14:45 +0530

    last commit

--- Image_Processing/Assignment2.m
+++ Image_Processing/Assignment2.m
@@ -1,15 +1,12 @@
 A = [5 2 4;
      1 7 -3;
      6 -10 0];
-
 B = [11 5 -3;
      0 -12 4;
-     2 6 1];
-     
+     2 6 1];     
 C = [7 14 1;
      10 3 -2;
-     8 -5 9];
- 
+     8 -5 9]; 
 a1 = A+B;
 a2 = B+A;
 if (a1==a2)
@@ -17,7 +14,6 @@ if (a1==a2)
 else
   printf("Both are different.\n");
 endif
-
 b1 = A + (B + C);
 b2 = (A + B) + C;
 if (b1==b2)
@@ -25,7 +21,6 @@ if (b1==b2)
 else
   printf("Both are different.\n");
 endif
-
 c1 = 5*(A + C);
 c2 = 5*A + 5*C;
 if (c1==c2)
@@ -33,7 +28,6 @@ if (c1==c2)
 else
   printf("Both are different.\n");
 endif
-
 d1 = A*(B+C);
 d2 = A*B + A*C;
 if (d1==d2)
